3724b8249cc349d2f0fc618ce9cb539c0e4308d6
Serranoda <112042205+Serranoda@users.noreply.github.com>
2024-09-09 08:13:09 -0700

    Add files via upload

--- ratnext/ratnext/src/app/page.tsx
+++ ratnext/ratnext/src/app/page.tsx
@@ -1,19 +1,38 @@
-import "./home.css"
+import "./home.css";
+import "./global.css";
 import Navbar from "../../components/navbar";
 import Image from "next/image";
+import Link from "next/link";
+
+
 
 function Home(){
     return(
-        <nav className="navbar">
-        <div className="navbar-container">
-            <div className="name-logo">
-                RAT<div className="logo" />
-            </div>
-            <div className="nav-menu">
+        <div>
+            <Navbar />
+            <div className="body">
+                <div className="body-image">
+                    <Image 
+                        src="/landingcartoon.png"
+                        height={500}
+                        width={500}
+                        alt="Landing page cartoon"
+                    />
+                </div>
 
+                <div className="text">
+                    <h1 className="rat-race">Getting into the rat race?</h1>
+                        <p className="text-body">
+                        Whether you’re a seasoned pro or just starting out, let’s connect you with your dream job enjoyably and efficiently.
+                        </p>
+                    <Link href='/' className="get-started-today">
+                        <h1>Get Started Today!</h1>                    
+                    </Link>
+
+                </div>
             </div>
         </div>
-    </nav>
+        
     );
 }
 
