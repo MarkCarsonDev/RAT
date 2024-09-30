9997a48dac8a6be30930062144b271e5627accb6
Serranoda <112042205+Serranoda@users.noreply.github.com>
2024-09-30 16:36:00 -0700

    import edit

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { LandingNavbar } from "@/ui/Landingnavbar";
+import { LandingNavbar } from "@/ui/LandingNavbar";
 import "@/ui/styling/LandingPage.css";
 import Image from "next/image";
 import { TextCarousel } from "@/ui/TextCarousel";
